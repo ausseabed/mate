fa3f947c0169884effbe39d91dffe8d4c8f71b39
Lachlan Hurst <lachlan.hurst@gmail.com>
2021-06-21 17:07:11 +1000

    fix broken unit test. Result of old refactoring to pass qajson objects instead of python dicts to check runner

--- tests/test_scan_check_runner.py
+++ tests/test_scan_check_runner.py
@@ -1,5 +1,6 @@
 import json
 import os
+from ausseabed.qajson.model import QajsonCheck
 import pytest
 import time
 import unittest
@@ -74,29 +75,33 @@ class TestMateCheckRunner(unittest.TestCase):
         """ Checks if the initialization has grouped the right checks under
         each file.
         """
-        checkrunner = CheckRunner(self.checks_json)
+        qajson_checks = [QajsonCheck.from_dict(d) for d in self.checks_json]
+        checkrunner = CheckRunner(qajson_checks)
         checkrunner.initialize()
 
-        file_one_checks = checkrunner._file_checks[('test/one.all', 'Raw Files')]
+        file_one_checks = checkrunner._file_checks[(
+            'test/one.all', 'Raw Files')]
 
         # make sure the right checks have been grouped under each file
         fn_check = next(
             (x for x in file_one_checks
-                if x['info']['id'] == "7761e08b-1380-46fa-a7eb-f1f41db38541"),
+                if x.info.id == "7761e08b-1380-46fa-a7eb-f1f41db38541"),
             None
         )
         dc_check = next(
             (x for x in file_one_checks
-                if x['info']['id'] == "4a3f3371-3a21-44f2-93cf-d9ed19d0c002"),
+                if x.info.id == "4a3f3371-3a21-44f2-93cf-d9ed19d0c002"),
             None
         )
         self.assertEqual(len(file_one_checks), 2)
         self.assertIsNotNone(fn_check)
         self.assertIsNotNone(dc_check)
 
-        file_two_checks = checkrunner._file_checks[('test/two.all', 'Raw Files')]
+        file_two_checks = checkrunner._file_checks[(
+            'test/two.all', 'Raw Files')]
         self.assertEqual(len(file_two_checks), 1)
-        file_three_checks = checkrunner._file_checks[('test/three.all', 'Raw Files')]
+        file_three_checks = checkrunner._file_checks[(
+            'test/three.all', 'Raw Files')]
         self.assertEqual(len(file_three_checks), 1)
 
 
